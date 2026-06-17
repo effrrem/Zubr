8d403205c41074f4a192e2dbd8d9b2d00ba961da
effrrem <matvey.efremov@internet.ru>
2026-06-17 15:54:42 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('Server on port ' + port));
